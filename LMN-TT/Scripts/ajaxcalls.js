853ae025e55ad9ac7ec93b140f18fb92ac92ae87
EZarkov <evstati.zarkov@gmail.com>
2014-08-26 18:53:18 +0300

    Adding ajax call script

--- LMN-TT/Scripts/ajaxcalls.js
+++ LMN-TT/Scripts/ajaxcalls.js
@@ -1,3 +1,13 @@
 /**
  * Created by Evstati on 14-8-26.
  */
+$(document).ready(function () {
+
+    $(".category").click(function () {
+        $.post('showCategory.php', {id: this.id}, function (response) {
+            // log the response to the console
+            console.log("Response: " + response);
+            $("#result").html(response);
+        });
+    });
+});
